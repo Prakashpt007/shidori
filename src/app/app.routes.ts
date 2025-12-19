9874abbcca2df77b43d8e4dc225c31746708f5f3
Prakashpt007 <prakashpt007@gmail.com>
2025-12-19 19:45:52 +0530

    login page added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+	{ path: "", redirectTo: "login", pathMatch: "full" },
+	{ path: "login", loadComponent: () => import('./core/authentication/login/login.component').then(c => c.LoginComponent), data: { title: "Login Page" } },
+	{ path: "register", loadComponent: () => import('./core/authentication/register/register.component').then(c => c.RegisterComponent), data: { title: "Register Page" } },
+	{
+		path: "**",
+		loadComponent: () => import('./core/error/error-404/error-404.component').then(c => c.Error404Component), data: { title: "Error 404 Page" }
+	}
+];
