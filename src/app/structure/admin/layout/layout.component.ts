587796d8b1ba9ac478ac2bf2529a90c1ae7d588f
Prakashpt007 <prakashpt007@gmail.com>
2026-02-12 17:07:34 +0530

    sidebar update for backend

--- src/app/structure/admin/layout/layout.component.ts
+++ src/app/structure/admin/layout/layout.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { SidebarComponent } from '../sidebar/sidebar.component';
+import { DOCUMENT } from '@angular/common';
 
 @Component({
 	selector: 'app-layout',
@@ -10,5 +11,25 @@ import { SidebarComponent } from '../sidebar/sidebar.component';
 	styleUrl: './layout.component.scss'
 })
 export class LayoutComponent {
+	constructor(@Inject(DOCUMENT) private document: Document) { }
 
+	ngOnInit() {
+		this.loadAdminTheme();
+	}
+
+	ngOnDestroy() {
+		const link = this.document.getElementById('admin-theme');
+		link?.remove();
+	}
+
+	private loadAdminTheme() {
+		let link = this.document.getElementById('admin-theme') as HTMLLinkElement;
+		if (link) return;
+
+		link = this.document.createElement('link');
+		link.id = 'admin-theme';
+		link.rel = 'stylesheet';
+		link.href = '/admin-theme.css';  // Auto-served by ng serve
+		this.document.head.appendChild(link);
+	}
 }
